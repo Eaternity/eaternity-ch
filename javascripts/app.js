7c1f8630d0d1a495410f6571ea9446f04e42d4fd
mklarmann <mklarmann@googlemail.com>
2012-01-13 16:01:32 +0100

    orbit implement + some fixes

--- javascripts/app.js
+++ javascripts/app.js
@@ -47,6 +47,28 @@ $(document).ready(function () {
 
 
 
+	/* jQuery Orbit ------------- */
+
+	$('#featured').orbit({
+	     animation: 'horizontal-push',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+	     animationSpeed: 1200,                // how fast animtions are
+	     timer: true, 			 // true or false to have the timer
+	     advanceSpeed: 6000, 		 // if timer is enabled, time between transitions 
+	     pauseOnHover: true, 		 // if you hover pauses the slider
+	     startClockOnMouseOut: true, 	 // if clock should start on MouseOut
+	     startClockOnMouseOutAfter: 1000, 	 // how long after MouseOut should the timer start again
+	     directionalNav: false, 		 // manual advancing directional navs
+	     captions: false, 			 // do you want captions?
+	     captionAnimation: 'fade', 		 // fade, slideOpen, none
+	     captionAnimationSpeed: 800, 	 // if so how quickly should they animate in
+	     bullets: true,			 // true or false to activate the bullet navigation
+	     bulletThumbs: false,		 // thumbnails for the bullets
+	     bulletThumbLocation: '',		 // location from this file where thumbs will be
+	     afterSlideChange: function(){}, 	 // empty function 
+	     fluid: true                         // or set a aspect ratio for content slides (ex: '4x3') 
+	});
+	
+	
 	/* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
 //	$('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
 //	$('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
