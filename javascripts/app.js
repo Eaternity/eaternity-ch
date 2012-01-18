402bc7dbf75f5fed26fb3be76fbac2adf38a51c2
mklarmann <mklarmann@googlemail.com>
2012-01-18 19:53:37 +0100

    tabs for recipes site

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,11 +7,13 @@ $(document).ready(function () {
 	/* Remove if you don't need :) */
 
 	function activateTab($tab) {
-		var $activeTab = $tab.closest('dl').find('a.active'),
+		var $activeTab = $tab.closest('.tabs').find('a.active'),
+			$activeTab2 = $tab.closest('dl').find('a.active'),
 				contentLocation = $tab.attr("href") + 'Tab';
 
 		//Make Tab Active
 		$activeTab.removeClass('active');
+		$activeTab2.removeClass('active');
 		$tab.addClass('active');
 
     	//Show Tab Content
@@ -19,6 +21,14 @@ $(document).ready(function () {
 		$(contentLocation).show();
 	}
 
+	$('div.tabs').each(function () {
+		//Get all tabs
+		var tabs = $(this).children('div').children('a');
+		tabs.click(function (e) {
+			activateTab($(this));
+		});
+	});
+		
 	$('dl.tabs').each(function () {
 		//Get all tabs
 		var tabs = $(this).children('dd').children('a');
@@ -30,6 +40,35 @@ $(document).ready(function () {
 	if (window.location.hash) {
 		activateTab($('a[href="' + window.location.hash + '"]'));
 	}
+
+	/* TABS --------------------------------- */
+	/* Remove if you don't need :) */
+
+	// function activateTab($tab) {
+	// 	var $activeTab = $tab.closest('dl').find('a.active'),
+	// 			contentLocation = $tab.attr("href") + 'Tab';
+	// 
+	// 	//Make Tab Active
+	// 	$activeTab.removeClass('active');
+	// 	$tab.addClass('active');
+	// 
+	//     	//Show Tab Content
+	// 	$(contentLocation).closest('.tabs-content').children('li').hide();
+	// 	$(contentLocation).show();
+	// }
+	// 
+	// $('dl.tabs').each(function () {
+	// 	//Get all tabs
+	// 	var tabs = $(this).children('dd').children('a');
+	// 	tabs.click(function (e) {
+	// 		activateTab($(this));
+	// 	});
+	// });
+	// 
+	// if (window.location.hash) {
+	// 	activateTab($('a[href="' + window.location.hash + '"]'));
+	// }
+
 
 	/* ALERT BOXES ------------ */
 	$(".alert-box").delegate("a.close", "click", function(event) {
@@ -67,6 +106,27 @@ $(document).ready(function () {
 	     afterSlideChange: function(){}, 	 // empty function 
 	     fluid: true                         // or set a aspect ratio for content slides (ex: '4x3') 
 	});
+	
+	
+	$('#featured-preview').orbit({
+	     animation: 'horizontal-push',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+	     animationSpeed: 800,                // how fast animtions are
+	     timer: true, 			 // true or false to have the timer
+	     advanceSpeed: 8000, 		 // if timer is enabled, time between transitions 
+	     pauseOnHover: false, 		 // if you hover pauses the slider
+	     startClockOnMouseOut: false, 	 // if clock should start on MouseOut
+	     startClockOnMouseOutAfter: 1000, 	 // how long after MouseOut should the timer start again
+	     directionalNav: false, 		 // manual advancing directional navs
+	     captions: true, 			 // do you want captions?
+	     captionAnimation: 'fade', 		 // fade, slideOpen, none
+	     captionAnimationSpeed: 800, 	 // if so how quickly should they animate in
+	     bullets: true,			 // true or false to activate the bullet navigation
+	     bulletThumbs: true,		 // thumbnails for the bullets
+	     bulletThumbLocation: '../images/recipes/preview/',		 // location from this file where thumbs will be
+	     afterSlideChange: function(){}, 	 // empty function 
+	     fluid: true                         // or set a aspect ratio for content slides (ex: '4x3') 
+	});
+	
 	
 	/* Something about Links??? ------------- */
 	$("#aside .thumbs a").click(
